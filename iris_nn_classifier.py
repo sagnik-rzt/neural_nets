96b49c2da7fd6b39e786fe3d844796fe1753c5fb
sagnik-rzt <sagnik.bhattacharya@razorthink.com>
2017-12-18 17:19:33 +0530

    Changed batch sampling in gradient descent, also changed get_data() function

--- iris_nn_classifier.py
+++ iris_nn_classifier.py
@@ -6,49 +6,39 @@
 
 import tensorflow as tf
 import numpy as np
+import pandas as pd
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 
-random_seed = 6
-tf.set_random_seed(random_seed)
+# random_seed = 6
+# tf.set_random_seed(random_seed)
 
-def forward_prop(x, w1, w2):
+def forward_prop(x, w1, w2, b1, b2):
     #Forward propagation of the input-data tensor 'x' across the layers of neurons
     #Activation function is the sigmoid or logistic function.
 
-    z1 = tf.matmul(x, w1)
+    z1 = tf.matmul(x, w1) + b1
     a1 = tf.nn.tanh(z1)
-    score = tf.matmul(a1, w2)
+    score = tf.matmul(a1, w2) + b2
     return score
 
-
 def get_data():
-    #Loads the Iris dataset and then splits them into training and test sets.
+    data = pd.read_csv(filepath_or_buffer = "iris_dataset.csv")
+    data.columns = ['x1', 'x2', 'x3', 'x4', 'y1', 'y2', 'y3']
+    train_set = data.take(np.random.permutation(len(data)) [:105])
+    x_train, y_train = train_set[['x1', 'x2', 'x3', 'x4']], train_set[['y1', 'y2', 'y3']]
+    test_set = data.take(np.random.permutation(len(data)) [:35])
+    x_test, y_test = test_set[['x1', 'x2', 'x3', 'x4']], test_set[['y1', 'y2', 'y3']]
 
-    iris = datasets.load_iris()
-    x_data = iris["data"]
-    y_target = iris["target"]
-
-    #Append a column full of 1s to the feature vector for the bias term
-    m, n = x_data.shape
-    X = np.ones((m, n + 1))
-    X[:, 1:] = x_data
-
-    #Convert target values to one-hot vectors
-    num_label_types = len(np.unique(y_target))
-    Y = np.eye(num_label_types)[y_target]
-
-    #Use 30% of the data as testing data and the rest as training data
-    return train_test_split(X, Y, test_size = 0.30, random_state = random_seed)
-
+    return x_train, x_test, y_train, y_test
 
 def main():
 
     #Now let's configure the neural network
-    #Total 5 input neurons; 4 feature neurons and 1 bias neuron
-    input_neurons = 5
+    #Total 4 input neurons
+    input_neurons = 4
     #50 neurons in the hidden layer
-    hidden_layers = 50
+    hidden_layers = 6
     #3 classes of Iris flowers, so 3 output neurons.
     output_neurons = 3
 
@@ -57,10 +47,11 @@ def main():
 
     W1 = tf.Variable(tf.random_normal(shape = [input_neurons, hidden_layers]), dtype = tf.float32)
     W2 = tf.Variable(tf.random_normal(shape = [hidden_layers, output_neurons]), dtype = tf.float32)
+    B1 = tf.Variable(tf.random_normal(shape = [1, hidden_layers]), dtype = tf.float32)
+    B2 = tf.Variable(tf.random_normal(shape = [1, output_neurons]), dtype = tf.float32)
 
     #Forward propagation
-    y_hat = forward_prop(X, W1, W2) #The hypothesis vector
-    y_predict = tf.cast(tf.argmax(y_hat, axis = 1), tf.float32)  #Getting the final score from the hypothesis vector
+    y_hat = forward_prop(X, W1, W2, B1, B2) #The hypothesis vector
 
     #Backward propagation
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = Y, logits = y_hat))
@@ -69,18 +60,14 @@ def main():
     #Now let's run the tensorflow session
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        batch_size = 14
+        x_train, x_test, y_train, y_test = get_data()
 
-        for epoch in range(1000):
-            x_train, x_test, y_train, y_test = get_data()
+        for epoch in range(100):
 
-            for i in range(len(x_train)):
+            for i in range(0, len(x_train), 10):
             #Perform mini-batch gradient descent for a mini-batch of size 'batch_size'
-                sess.run(update_weights, feed_dict = {X : x_train[i : i+batch_size] , Y : y_train[i : i+batch_size]})
+                sess.run(update_weights, feed_dict = {X : x_train[i : i+10] , Y : y_train[i : i+10]})
 
-            training_accuracy = np.mean(np.argmax(y_train, axis = 1) == sess.run(y_predict, feed_dict = {X : x_train, Y : y_train}))
-            testing_accuracy =  np.mean(np.argmax(y_test, axis = 1) == sess.run(y_predict, feed_dict = {X : x_test, Y : y_test}))
-
-            print("epoch = %d, training_accuracy = %f, testing_accuracy = %f"%(epoch, training_accuracy, testing_accuracy))
+            print("epoch = %d, cost = %f"%(epoch, sess.run(cost, feed_dict = {X : x_train, Y : y_train})))
 
 main()
