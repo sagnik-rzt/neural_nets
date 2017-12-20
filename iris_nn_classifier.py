d63dacfc28c5e9a78046f011ffc7bd772d256c59
sagnik-rzt <sagnik.bhattacharya@razorthink.com>
2017-12-20 11:40:43 +0530

    some changes in batching

--- iris_nn_classifier.py
+++ iris_nn_classifier.py
@@ -56,6 +56,9 @@ def main():
 
     #Forward propagation
     y_hat = forward_prop(X, W1, W2, B1, B2) #The hypothesis vector
+    #Note that here we are not using a softmax function on y_hat since
+    # tf.nn.softmax_cross_entropy_with_logits() does that for us.
+
 
     #Backward propagation
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = Y, logits = y_hat))
@@ -70,7 +73,7 @@ def main():
 
         for epoch in range(1000):
 
-            for i in range(0, len(x_train), batch_size):
+            for i in range(0, len(x_train) - batch_size, batch_size):
             #Perform mini-batch gradient descent for a mini-batch of size 'batch_size'
                 sess.run(update_weights, feed_dict = {X : x_train[i : i+batch_size] , Y : y_train[i : i+batch_size]})
 
